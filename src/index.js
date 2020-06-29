179194c3a039d435409f7ba78e258a55d757b7bd
anshai <anshailol1@gmail.com>
2020-06-29 23:51:13 +0300

    set up docker, live reloading still missing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+const MongoClient = require('mongodb').MongoClient;
 const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const port = 5000;
+const dbName = 'testDataBaseBoi';
+const url = `mongodb://127.0.0.1:2717/${dbName}`;
+
 app.use(cors());
 
 app.get('/', (req, res, next) => {
-    res.send("lmao nice");
+    console.log('got the reqest');
+    console.log(req);
+    res.send('nice');
 });
 
-const server = app.listen(3000, () => {
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true});
 
+client.connect(err => {
+    console.log('here trying to connect');
+    if(err){
+        console.log('error yo');
+        console.dir(err);
+    } else {
+        console.log('no err yo');
+        const db = client.db(dbName);
+        db.collection('testcoll2').insertOne({iHope2: 'it works23'});
+    }
+});
+
+const server = app.listen(port, () => {
+    console.log(`server running on ${port}`);
 });
