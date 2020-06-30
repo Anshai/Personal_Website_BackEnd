b484437f60ea8399e30f537fefaa56df1b04e122
anshai <anshailol1@gmail.com>
2020-07-01 00:08:27 +0300

    finished the docker setup with docker-compose and hot reload

--- src/index.js
+++ src/index.js
@@ -3,32 +3,36 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-const port = 5000;
-const dbName = 'testDataBaseBoi';
-const url = `mongodb://127.0.0.1:2717/${dbName}`;
 
-app.use(cors());
+class App {
+    static init(){
 
-app.get('/', (req, res, next) => {
-    console.log('got the reqest');
-    console.log(req);
-    res.send('nice');
-});
+        this.port = 4200;
+        this.dbName = 'testDataBaseBoi';
+        this.url = `mongodb://127.0.0.1:2717/${this.dbName}`;
+        
+        app.use(cors());
+        
+        let server;
+        const client = new MongoClient(this.url, { useNewUrlParser: true, useUnifiedTopology: true});
 
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true});
-
-client.connect(err => {
-    console.log('here trying to connect');
-    if(err){
-        console.log('error yo');
-        console.dir(err);
-    } else {
-        console.log('no err yo');
-        const db = client.db(dbName);
-        db.collection('testcoll2').insertOne({iHope2: 'it works23'});
+        client.connect(err => {
+            console.log('Connecting to the database...');
+            if(err){
+                console.log('Failed to connect to the database...\nThe app will exit now...');
+                console.dir(err);
+            } else {
+                console.log('Succesfuly connected...');
+                const db_ref = client.db(this.dbName);
+        
+                server = app.listen(this.port, () => {
+                    console.log(`Server running on port: ${this.port}`);
+                });
+            }
+        });
     }
-});
+}
 
-const server = app.listen(port, () => {
-    console.log(`server running on ${port}`);
-});
+App.init();
+
+
